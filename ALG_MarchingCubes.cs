2d4f9e238e62f203d3cb2dc6779e6d4c7f529ebe
Albert Li <albertlidesign@gmail.com>
2020-03-04 17:41:17 +0800

    添加项目文件。

---
+++ ALG_MarchingCubes.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using Grasshopper.Kernel.Types;
+using System.Drawing;
+
+namespace ALG_MarchingCubes_GPU
+{
+    public class ALG_MC : GH_Component
+    {
+        public ALG_MC()
+          : base("MarchingCubes(ALG)", "ALG_MC", "Create mesh pipes from lines.", "ALG", "MarchingCubes") { }
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGeometryParameter("Geometries", "G", "Geometries", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Scale", "S", "Scale", GH_ParamAccess.item,1.2);
+            pManager.AddNumberParameter("isoValue", "ISO", "isoValue", GH_ParamAccess.item,5.0);
+            pManager.AddIntegerParameter("Count", "Count", "Count", GH_ParamAccess.item);
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddMeshParameter("Mesh", "M", "Mesh", GH_ParamAccess.item);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            List<IGH_GeometricGoo> geos = new List<IGH_GeometricGoo>();
+            List<IGH_GeometricGoo> new_geos = new List<IGH_GeometricGoo>();
+            List<double> Weights = new List<double>();
+            List<Point3d> samplePoints = new List<Point3d>();
+            double scale = 1.0;
+            double size = 1.0;
+            double isovalue = 5.0;
+            int count = 0;
+
+            DA.GetDataList("Geometries",  geos);
+            DA.GetData("Scale", ref scale);
+            DA.GetData("isoValue", ref isovalue);
+            DA.GetData("Count", ref count);
+
+            //建立基box
+            Box box1 = BasicFunctiosn.CreateUnionBBoxFromGeometry(geos, scale);
+            Plane plane = new Plane(Point3d.Origin, Vector3d.XAxis, Vector3d.YAxis);
+            Interval interval = new Interval(0, 1);
+
+            //建立映射目标box
+            Box box2 = new Box(plane, interval, interval, interval);
+
+            //开始映射
+            for (int i = 0; i < geos.Count; i++)
+            {
+                new_geos.Add(BasicFunctiosn.BoxMapping(box1, box2, geos[i]));
+            }
+
+            ////求三个方向上单元的数量
+            //Interval xD = box1.X;
+            //Interval yD = box1.Y;
+            //Interval zD = box1.Z;
+
+            //int xCount = (int)Math.Abs(Math.Round((xD.T1 - xD.T0)/ size, MidpointRounding.AwayFromZero));
+            //int yCount = (int)Math.Abs(Math.Round((yD.T1 - yD.T0) / size, MidpointRounding.AwayFromZero));
+            //int zCount = (int)Math.Abs(Math.Round((zD.T1 - zD.T0) / size, MidpointRounding.AwayFromZero));
+
+            //转换几何数据为点数据
+            samplePoints = BasicFunctiosn.ConvertGeosToPoints(new_geos);
+
+            //初始化网格数据
+            List<Point3d> meshVs = new List<Point3d>();
+
+            //开始计算MC
+            for (int X = 0; X < count; X++)
+            {
+                for (int Y = 0; Y < count; Y++)
+                {
+                    for (int Z = 0; Z < count; Z++)
+                    {
+                        List<Point3d> pts = new List<Point3d>();
+                        pts = MarchingCubes_GPU.MarchCube(isovalue, X * (1.0 / count), Y * (1.0 / count), Z * (1.0 / count), (1.0 / count), samplePoints, Weights);
+                        if (pts != null)
+                        {
+                            foreach (var item in pts)
+                            {
+                                meshVs.Add(item);
+                            }
+                        }
+                    }
+                }
+            }
+
+            Mesh mesh = BasicFunctiosn.ExtractMesh(meshVs);
+            GH_Mesh ghm = new GH_Mesh(mesh);
+            IGH_GeometricGoo geoResult = BasicFunctiosn.BoxMapping(box2, box1, ghm);
+            GH_Convert.ToMesh(geoResult, ref mesh, GH_Conversion.Both);
+
+            DA.SetData(0, mesh);
+        }
+        protected override Bitmap Icon => null;
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("{F4D2FDE0-365A-46B5-9AC3-3A4939D5E61A}"); }
+        }
+    }
+}
---
+++ MarchingCubes_GPU.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Grasshopper.Kernel.Types.Transforms;
+using Rhino.Geometry;
+
+namespace ALG_MarchingCubes_GPU
+{
+    public class BasicFunctiosn
+    {
+        //构建用于映射的Box
+        public static Box CreateUnionBBoxFromGeometry(List<IGH_GeometricGoo> geos, double scale)
+        {
+            Plane worldXY = Plane.WorldXY;
+            Transform xform = Transform.ChangeBasis(Plane.WorldXY, worldXY);
+            BoundingBox empty = BoundingBox.Empty;
+            int num = geos.Count - 1;
+            for (int i = 0; i <= num; i++)
+            {
+                if (geos[i] != null)
+                {
+                    BoundingBox boundingBox = geos[i].GetBoundingBox(xform);
+                    empty.Union(boundingBox);
+                }
+            }
+
+            Transform xform2 = Transform.Scale(empty.Center, scale);
+            empty.Transform(xform2);
+            Box box = new Box(empty);
+            return box;
+        }
+        //Box映射
+        public static IGH_GeometricGoo BoxMapping(Box box1, Box box2, IGH_GeometricGoo geo)
+        {
+
+            if (!box1.IsValid || !box2.IsValid)
+            {
+                return null;
+            }
+            Plane plane1 = box1.Plane;
+            Plane plane2 = box2.Plane;
+            plane1.Origin = box1.Center;
+            plane2.Origin = box2.Center;
+            double xscale = box2.X.Length / box1.X.Length;
+            double yscale = box2.Y.Length / box1.Y.Length;
+            double zscale = box2.Z.Length / box1.Z.Length;
+            ITransform item = new Scale(plane1, xscale, yscale, zscale);
+            ITransform item2 = new Orientation(plane1, plane2);
+            GH_Transform gh_Transform = new GH_Transform();
+            gh_Transform.CompoundTransforms.Add(item);
+            gh_Transform.CompoundTransforms.Add(item2);
+            gh_Transform.ClearCaches();
+
+            IGH_GeometricGoo geo2 = geo.DuplicateGeometry();
+            geo2 = geo2.Transform(gh_Transform.Value);
+            return geo2;
+        }
+
+        public static List<IGH_GeometricGoo> ConvertPointsToGeo(List<Point3d> pts)
+        {
+            List<IGH_GeometricGoo> geos = new List<IGH_GeometricGoo>();
+            for (int i = 0; i < pts.Count; i++)
+            {
+                GH_Point point = new GH_Point(pts[i]);
+                geos.Add(point);
+            }
+            return geos;
+        }
+        public static List<Point3d> ConvertGeosToPoints(List<IGH_GeometricGoo> geos)
+        {
+            List<Point3d> pts = new List<Point3d>();
+            for (int i = 0; i < geos.Count; i++)
+            {
+                if (geos[i].IsValid)
+                {
+                    Point3d point = new Point3d();
+                    GH_Convert.ToPoint3d(geos[i], ref point, GH_Conversion.Both);
+                    pts.Add(point);
+                }
+            }
+            return pts;
+        }
+        public static Mesh ExtractMesh(List<Point3d> pts)
+        {
+            Mesh mesh = new Mesh();
+            int FCount = pts.Count / 3;
+            for (int i = 0; i < FCount; i++)
+            {
+                Point3d a = pts[i * 3];
+                Point3d b = pts[i * 3 + 1];
+                Point3d c = pts[i * 3 + 2];
+
+                Mesh subMesh = new Mesh();
+                subMesh.Vertices.Add(a);
+                subMesh.Vertices.Add(b);
+                subMesh.Vertices.Add(c);
+                subMesh.Faces.AddFace(0, 1, 2);
+
+                mesh.Append(subMesh);
+            }
+            return mesh;
+        }
+    }
+
+        public class MarchingCubes_GPU
+    {
+        private static double[,] Vertices = new double[8, 3]
+          {
+             {0.0, 0.0, 0.0},{1.0, 0.0, 0.0},{1.0, 1.0, 0.0},{0.0, 1.0, 0.0},
+             {0.0, 0.0, 1.0},{1.0, 0.0, 1.0},{1.0, 1.0, 1.0},{0.0, 1.0, 1.0}
+           };
+        private static int[,] EdgeConnection = new int[12, 2]
+          {
+             {0,1}, {1,2}, {2,3}, {3,0},
+             {4,5}, {5,6}, {6,7}, {7,4},
+             {0,4}, {1,5}, {2,6}, {3,7}
+          };
+        private static double[,] EdgeDirection = new double[12, 3]
+          {
+            {1.0, 0.0, 0.0},{0.0, 1.0, 0.0},{-1.0, 0.0, 0.0},{0.0, -1.0, 0.0},
+            {1.0, 0.0, 0.0},{0.0, 1.0, 0.0},{-1.0, 0.0, 0.0},{0.0, -1.0, 0.0},
+            {0.0, 0.0, 1.0},{0.0, 0.0, 1.0},{ 0.0, 0.0, 1.0},{0.0, 0.0, 1.0}
+          };
+        private static Point3d[] EdgeVertex = new Point3d[12];
+        private static Point3d[] EdgeNorm = new Point3d[12];
+
+
+        public static double Dist(double X, double Y, double Z, List<Point3d> SamplePoints, List<double> Weights)
+        {
+            double result = 0.0;
+            double Dx, Dy, Dz;
+            double sum = 0.0;
+            foreach (var item in Weights)
+            {
+                sum += item;
+            }
+
+            for (int i = 0; i < SamplePoints.Count; i++)
+            {
+                Dx = X - SamplePoints[i].X;
+                Dy = Y - SamplePoints[i].Y;
+                Dz = Z - SamplePoints[i].Z;
+
+                result += (sum * (Weights[i] / sum)) / (Dx * Dx + Dy * Dy + Dz * Dz);
+            }
+            return result;
+        }
+
+        public static double GetOffset(double Value1, double Value2, double ValueDesired)
+        {
+            if ((Value2 - Value1) == 0.0)
+                return 0.5;
+
+            return (ValueDesired - Value1) / (Value2 - Value1);
+        }
+
+        public static List<Point3d> MarchCube(double isovalue, double fx, double fy, double fz, double Scale, List<Point3d> SamplePoints, List<double> Weights)
+        {
+            //检查权重
+            if (Weights.Count < SamplePoints.Count)
+            {
+                List<double> average = new List<double>();
+                for (int i = 0; i < SamplePoints.Count; i++)
+                {
+                    average.Add(1);
+                }
+                Weights = average;
+            }
+
+            List<Point3d> pts = new List<Point3d>();
+            double[] CubeValues = new double[8];
+            double Offset = 0.0;
+            int flag = 0;
+            int EdgeFlag = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                //计算CubeValue
+                CubeValues[i] = Dist(fx + Vertices[i, 0] * Scale,
+                  fy + Vertices[i, 1] * Scale,
+                  fz + Vertices[i, 2] * Scale, SamplePoints, Weights);
+
+                //判定顶点状态
+                if (CubeValues[i] <= isovalue)
+                {
+                    flag |= 1 << i;
+                }
+            }
+            //找到哪些几条边和边界相交
+            EdgeFlag = Tables.CubeEdgeFlags[flag];
+
+
+            //如果整个立方体都在边界内，则没有交点
+            if (EdgeFlag == 0) return null;
+
+            //找出每条边和边界的相交点，找出在这些交点处的法线量
+            for (int i = 0; i < 12; i++)
+            {
+                if ((EdgeFlag & (1 << i)) != 0) //如果在这条边上有交点
+                {
+                    Offset = GetOffset(CubeValues[EdgeConnection[i, 0]], CubeValues[EdgeConnection[i, 1]], isovalue);
+
+                    EdgeVertex[i].X = fx + (Vertices[EdgeConnection[i, 0], 0] + Offset * EdgeDirection[i, 0]) * Scale;
+                    EdgeVertex[i].Y = fy + (Vertices[EdgeConnection[i, 0], 1] + Offset * EdgeDirection[i, 1]) * Scale;
+                    EdgeVertex[i].Z = fz + (Vertices[EdgeConnection[i, 0], 2] + Offset * EdgeDirection[i, 2]) * Scale;
+                }
+            }
+
+            //画出找到的三角形
+            for (int Triangle = 0; Triangle < 5; Triangle++)
+            {
+                if (Tables.TriangleConnectionTable[flag, 3 * Triangle] < 0)
+                    break;
+
+
+                for (int Corner = 0; Corner < 3; Corner++)
+                {
+                    int Vertex = Tables.TriangleConnectionTable[flag, 3 * Triangle + Corner];
+                    Point3d pd = new Point3d(EdgeVertex[Vertex].X, EdgeVertex[Vertex].Y, EdgeVertex[Vertex].Z);
+                    pts.Add(pd);
+                }
+            }
+            return pts;
+        }
+    }
+}
